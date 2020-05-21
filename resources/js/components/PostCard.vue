8ec52473d499fc18ebc91ffb38b278a7c5ee418b
Alan Hardman <alanaktion@gmail.com>
2020-05-21 10:00:21 -0600

    Use relative date on posts

--- resources/js/components/PostCard.vue
+++ resources/js/components/PostCard.vue
@@ -10,9 +10,10 @@
                     :to="`/@${post.user.username}`">
                     {{ post.user.name }}
                 </router-link>
-                <router-link class="block text-gray-600 dark:text-gray-400"
-                    :to="`/posts/${post.id}`">
-                    {{ postDate }}
+                <router-link class="block text-gray-500"
+                    :to="`/posts/${post.id}`"
+                    :title="fullDate">
+                    {{ relativeDate }}
                 </router-link>
             </div>
         </div>
@@ -21,14 +22,54 @@
 </template>
 
 <script>
+function prettyDate(date) {
+    var diff = ((Date.now() - date.getTime()) / 1000),
+        day_diff = Math.floor(diff / 86400);
+
+    if (isNaN(day_diff) || day_diff < 0 || day_diff >= 31) {
+        return date.toLocaleString();
+    }
+
+    return day_diff == 0 && (
+            diff < 60 && 'just now' ||
+            diff < 120 && '1 minute ago' ||
+            diff < 3600 && Math.floor(diff / 60) + ' minutes ago' ||
+            diff < 7200 && '1 hour ago' ||
+            diff < 86400 && Math.floor(diff / 3600) + ' hours ago'
+        ) ||
+        day_diff == 1 && 'Yesterday' ||
+        day_diff < 7 && day_diff + ' days ago' ||
+        day_diff < 31 && Math.ceil(day_diff / 7) + ' weeks ago';
+}
+
 export default {
     props: {
         post: Object,
     },
     computed: {
-        postDate() {
+        fullDate() {
             const date = new Date(this.$props.post.created_at);
             return date.toLocaleString();
+        },
+        relativeDate() {
+            const date = new Date(this.$props.post.created_at);
+            const diff = ((Date.now() - date.getTime()) / 1000);
+            const dayDiff = Math.floor(diff / 86400);
+
+            if (isNaN(dayDiff) || dayDiff < 0 || dayDiff >= 31) {
+                return date.toLocaleString();
+            }
+
+            return dayDiff == 0 && (
+                    diff < 60 && 'just now' ||
+                    diff < 120 && '1 minute ago' ||
+                    diff < 3600 && Math.floor(diff / 60) + ' minutes ago' ||
+                    diff < 7200 && '1 hour ago' ||
+                    diff < 86400 && Math.floor(diff / 3600) + ' hours ago'
+                ) ||
+                dayDiff == 1 && 'Yesterday' ||
+                dayDiff < 7 && dayDiff + ' days ago' ||
+                dayDiff < 31 && Math.ceil(dayDiff / 7) + ' weeks ago';
         },
     },
 };
